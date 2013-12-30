c10b30c4f4d9a121b0be4befbc99cfa737d0cfa8
XiongZhijun <hust.xzj@gmail.com>
2013-12-30 10:38:26 +0800

    解决bug，更新依赖Android版本

--- house-tools/src/org/herod/house/SettingActivity.java
+++ house-tools/src/org/herod/house/SettingActivity.java
@@ -5,7 +5,6 @@
  */
 package org.herod.house;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
@@ -14,12 +13,13 @@ import android.preference.PreferenceFragment;
  * @author Xiong Zhijun
  * 
  */
-@SuppressLint("NewApi")
 public class SettingActivity extends PreferenceActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+		getFragmentManager().beginTransaction()
+				.replace(android.R.id.content, new MyPreferenceFragment())
+				.commit();
 	}
 
 	public static class MyPreferenceFragment extends PreferenceFragment {
